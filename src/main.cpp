fccd11f058f13adfad46fbb3340b03374a5046bb
Gereon V <72784429+GereonV@users.noreply.github.com>
2022-06-27 17:00:49 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+int main() {
+    if(!glfwInit()) return -1;
+    GLFWwindow * window = glfwCreateWindow(1280, 720, "Template-Titel", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init();
+    ImGui::GetIO().IniFilename = nullptr;
+
+    glClearColor(.4f, .6f, 1.f, 1.f);
+    while(!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::ShowDemoWindow();
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwTerminate();
+}
