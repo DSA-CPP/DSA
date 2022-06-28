bc6bb8b218d06e5c308076a2091b1008b1a24d9b
Gereon V <72784429+GereonV@users.noreply.github.com>
2022-06-28 02:23:58 +0200

    Add header for type declarations

---
+++ src/types.hpp
@@ -0,0 +1,14 @@
+#ifndef _DSA_TYPES_HPP_
+#define _DSA_TYPES_HPP_
+
+#include <cstdint>
+
+namespace dsa::types {
+
+    using age_type = typename std::uint8_t;
+    using count_type = typename std::uint8_t;
+    using entry_type = typename std::uint16_t;
+
+}
+
+#endif
