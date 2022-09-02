b725bdf97d739f58613e91f67c809e278fce24b4
Gereon V <72784429+GereonV@users.noreply.github.com>
2022-09-02 20:53:33 +0200

    Comply with Dear ImGui's licensing

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <GLFW/glfw3.h>
-#include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
 #include "disciplines.hpp"
 
 int main() {
