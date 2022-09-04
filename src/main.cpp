2a433cc7c9b428ee25e2cf321fe075fc95da471e
Gereon V <72784429+GereonV@users.noreply.github.com>
2022-09-05 00:00:25 +0200

    Refactor GUI-Code (RAII)

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,80 @@
+#include <stdexcept>
 #include <GLFW/glfw3.h>
 #include <imgui/imgui.h>
 #include <imgui/imgui_impl_glfw.h>
 #include <imgui/imgui_impl_opengl3.h>
-#include "disciplines.hpp"
+#include "client.hpp"
 
-int main() {
-    if(!glfwInit()) return -1;
-    GLFWwindow * window = glfwCreateWindow(1280, 720, "Template-Titel", NULL, NULL);
-    glfwMakeContextCurrent(window);
-
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init();
-    ImGui::GetIO().IniFilename = nullptr;
-
-    glClearColor(.4f, .6f, 1.f, 1.f);
-    while(!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
-
-        ImGui::ShowDemoWindow();
-
-        ImGui::Render();
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+class glfw_context {
+private:
+    static inline glfw_context * current{};
+public:
+    glfw_context() {
+        if(current)
+            throw std::logic_error{"double context-creation"};
+        if(!glfwInit())
+            throw std::runtime_error{"glfwInit failed"};
+        current = this;
     }
 
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
+    ~glfw_context() {
+        glfwTerminate();
+        current = nullptr;
+    }
 
-    glfwTerminate();
+    static bool exists() noexcept { return current; }
+};
+
+class imgui_context {
+private:
+    static inline imgui_context * current{};
+public:
+    imgui_context(char const * title, int width, int height) {
+        if(!glfw_context::exists())
+            throw std::logic_error{"no glfw-context"};
+        if(current)
+            throw std::logic_error{"double context-creation"};
+        window_ = glfwCreateWindow(width, height, title, nullptr, nullptr);
+        glfwMakeContextCurrent(window_);
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGui_ImplGlfw_InitForOpenGL(window_, true);
+        ImGui_ImplOpenGL3_Init();
+        current = this;
+    }
+
+    ~imgui_context() {
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
+        current = nullptr;
+    }
+
+    static void loop(auto f) {
+        while(!glfwWindowShouldClose(current->window_)) {
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+            f(current->window_);
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+            glfwSwapBuffers(current->window_);
+            glfwPollEvents();
+        }
+    }
+
+    static bool exists() noexcept { return current; }
+private:
+    GLFWwindow * window_;
+};
+
+int main() {
+    glfw_context glfw_ctx;
+    imgui_context ig_ctx{"Template-Titel", 1280, 720};
+    ImGui::GetIO().IniFilename = nullptr;
+    glClearColor(.4f, .6f, 1.f, 1.f);
+    ig_ctx.loop([](auto) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui::ShowDemoWindow();
+    });
 }
