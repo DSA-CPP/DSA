92239d0b53da19e0f15a96faf418a5ca0a93a27c
Gereon V <72784429+GereonV@users.noreply.github.com>
2022-10-12 20:57:26 +0200

    Remove unnecessary functions

--- src/server.hpp
+++ src/server.hpp
@@ -11,19 +11,9 @@ namespace dsa::server {
 
     struct participant_data {}; // TODO add fields
 
-    template<typename T, typename U, typename V, std::size_t Extent>
-    inline std::vector<std::tuple<T, U, V>> merge(std::span<std::pair<T, U> const, Extent> vals, std::unordered_map<T, V> const & map) {
-        std::vector<std::tuple<T, U, V>> res;
-        res.reserve(vals.size());
-        for(auto && [t, u] : vals)
-            res.emplace_back(t, u, map.at(t));
-        return res;
-    }
-
     class context {
     using pair = std::pair<entry_type, participant>;
     public:
-        context() noexcept {} // TODO load
         auto participants() const noexcept {
             std::vector<std::tuple<entry_type, participant, participant_data>> res;
             res.reserve(parts_.size());
@@ -44,6 +34,7 @@ namespace dsa::server {
         }
 
     private:
+        // TODO load
         std::unordered_map<entry_type, participant_data> data_;
         std::vector<pair> parts_; // network order
     };
