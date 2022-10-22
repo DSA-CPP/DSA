e57174e4c9d1b084e343a72ad611a6bcf9c9fccd
Gereon V <72784429+GereonV@users.noreply.github.com>
2022-10-23 00:05:20 +0200

    Add small wrappers

--- src/server.cpp
+++ src/server.cpp
@@ -1,3 +1,12 @@
 #include "server.hpp"
 
+static void values_in(net::tcp::connection const & conn) {
+    auto [id, vals] = dsa::receive_values(conn);
+    dsa::server::file(id).save(vals);
+}
+
+static void values_out(net::tcp::connection const & conn) {
+    dsa::server::participant_values{dsa::detail::recv<dsa::entry_type>(conn)}.send(conn);
+}
+
 int main() {}
